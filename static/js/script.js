d30e0e5c8be56556df57e580a113b4eba7a861d3
liammcluckie <liammcluckie@hotmail.co.uk>
2021-05-20 18:18:53 +0000

    Bind data from db to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
   });
 
-$(document).ready(function(){
-  $('.collapsible').collapsible();
-});
+
+
